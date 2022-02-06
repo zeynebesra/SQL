48deafaca5524830026459a5c736e1d49b751576
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-02-06 20:38:19 +0300

    ödev 5 eklendi

---
+++ odev6.sql
@@ -0,0 +1,12 @@
+-- ÖDEV 6
+
+-- 1-)film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+
+
+-- 2-)film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+
+
+-- 3-)film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+
+
+-- 4-)film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
---
+++ odev7.sql
@@ -0,0 +1,9 @@
+-- ÖDEV 7
+
+-- 1.film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+
+-- 2.film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+
+-- 3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+
+-- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
